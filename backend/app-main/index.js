3ddb5dfa97ccad9fb387be24854f11deb20e4008
elisedare28 <125211920+elisedare28@users.noreply.github.com>
2024-08-24 19:17:54 +0530

    origin updated

--- backend/app-main/index.js
+++ backend/app-main/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: [""],
+    origin: ["https://productivity-manager-new.vercel.app"],
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true
 }));
