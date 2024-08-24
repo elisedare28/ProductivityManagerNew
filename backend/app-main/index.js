ac13bb94009cec1961b9e1b05b769a51278ec08a
elisedare28 <125211920+elisedare28@users.noreply.github.com>
2024-08-24 19:14:14 +0530

    origin

--- backend/app-main/index.js
+++ backend/app-main/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: 'https://website.com',
+    origin: [""],
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true
 }));
